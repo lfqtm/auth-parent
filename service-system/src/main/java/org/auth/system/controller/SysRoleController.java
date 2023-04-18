db650be7b81c3a5833f810c054abd6888de83871
lfqtm <740942486@qq.com>
2023-04-18 12:06:53 +0800

    feature(main): #3 集成knife4j

--- service-system/src/main/java/org/auth/system/controller/SysRoleController.java
+++ service-system/src/main/java/org/auth/system/controller/SysRoleController.java
@@ -1,5 +1,7 @@
 package org.auth.system.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.auth.model.system.SysRole;
 import org.auth.system.service.SysRoleService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
+@Api(tags = "角色管理")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -16,6 +19,7 @@ public class SysRoleController {
     @Resource
     private SysRoleService sysRoleService;
 
+    @ApiOperation(value = "获取全部角色列表")
     @GetMapping("/findAll")
     public List<SysRole> findAll() {
         List<SysRole> roleList = sysRoleService.list();
